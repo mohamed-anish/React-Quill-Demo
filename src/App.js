759d30143bd2edf6d09f2f5ad4de40fc4444915c
mohamed-anish <anishsina1995.14@gmail.com>
2024-02-01 22:49:29 +0530

    my last commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import Quill from 'quill';
-
-// Import Quill modules
 import 'quill-emoji/dist/quill-emoji.css';
-// import 'quill-find-replace/dist/quill.findreplace.css';
+// import 'quill-find-replace/dist/quill.findreplace.css'; //this module is not found in npm
 import 'quill-better-table/dist/quill-better-table.css';
 
 // Import Quill modules as needed
 import QuillEmoji from 'quill-emoji';
-// import QuillFindReplace from 'quill-find-replace';
+// import QuillFindReplace from 'quill-find-replace'; //this module is not found in npm
 import QuillCursors from 'quill-cursors';
 import QuillBetterTable from 'quill-better-table';
 import htmlEditButton from "quill-html-edit-button";
-// import quillFocus from 'quill-focus';
 import Focus from 'quill-focus';
 
-// Register Quill modules
+
 Quill.register('modules/emoji', QuillEmoji);
-// Quill.register('modules/find-replace', QuillFindReplace);
+// Quill.register('modules/find-replace', QuillFindReplace); //this module is not found in npm 
 Quill.register('modules/cursors', QuillCursors);
 Quill.register('modules/better-table', QuillBetterTable);
 Quill.register('modules/htmlEditButton', htmlEditButton)
@@ -43,7 +40,7 @@ const toolbarOptions = [
   [{ 'color': [] }, { 'background': [] }],
   [{ 'font': [] }],
   [{ 'align': [] }],
-  [{'table':['1','2','3','4']}]
+  ['table'],
   
 ];
 
@@ -51,33 +48,40 @@ const QuillEditor = () => {
   const [editorHtml, setEditorHtml] = useState('');
   const [editorHtmlValue,setEditorHtmlValue] = useState('')
 
-  let editor = null;
-
   const handleChange = (e) => {
     setEditorHtml(e);
   };
 
-  const addTable = (e) => {
-    e.preventDefault();
-    // Quill.getModule("better-table").insertTable(3,3);
-    this.quillRef.getEditor().getModule('better-table');
+const reactQuillRef =useRef(null);
 
-}
+const insertTable = () => {
+  const editor = reactQuillRef.current.getEditor();
+  const tableModule = editor.getModule("better-table");
+  tableModule.insertTable(3, 3);
+};
+
+ useEffect(() => {
+  const editor = reactQuillRef.current.getEditor();
+  const toolbar = editor.getModule("toolbar");
+  console.log(toolbar)
+  toolbar.addHandler("table", () => {
+    insertTable();
+  });
+}, [])
 
 
   return (
     <div style={{height:"500px",width:"800px",margin:"0px auto",}}>
       <h1 style={{textAlign:"center"}}>React Quill Text Editor</h1>
-      <button onClick={(event) =>addTable(event)}>Add Table</button>
       <ReactQuill
         theme="snow"
-        ref={(el) =>{editor = el}}
+        ref={reactQuillRef}
         placeholder='Write something here...'
         modules={{
           toolbar: toolbarOptions,
           'emoji-toolbar': true,
           // 'better-table': true,
-          table:false,
+          // table:false,
           // 'better-table': {
           //   operationMenu:{
           //     items:{
@@ -128,7 +132,7 @@ const QuillEditor = () => {
       />
       <button 
       onClick={()=>{setEditorHtmlValue(editorHtml)}}
-      style={{color:'yellow',backgroundColor:"grey",height:'30px',borderRadius:"4px",margin:"5px 0"}} >Submit Content</button>
+      style={{color:'black',backgroundColor:"aqua",height:'30px',borderRadius:"4px",margin:"5px 0"}} >Submit Content</button>
       <h2>Display Typing Content here below:  </h2>
       <div dangerouslySetInnerHTML={{__html: editorHtmlValue}}>
       </div>
